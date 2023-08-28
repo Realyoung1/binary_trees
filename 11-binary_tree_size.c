1d2e0cd562f01bc74da960d3461a6a695523fba8
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-08-28 07:17:34 -0700

    binary tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,10 +1,11 @@
 #include "binary_trees.h"
 
 /**
-  * binary_tree_size - measures the size of a binary tree
-  * @tree: root node of the BT
-  * Return: the number of nodes
+  * binary_tree_size - funcs measuring the size of a binary treess
+  * @tree: rootss node of the BT
+  * Return: the numbesr of node
   */
+
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	size_t size = 0;

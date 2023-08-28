c66a9ba916633a2ce8020a35ee24758fd11f8b57
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-08-28 07:16:34 -0700

    binary tree

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,10 +1,11 @@
 #include "binary_trees.h"
 
 /**
-  * node_depth - count the levels of the Binary tree
-  * @tree: root node of the BT
+  * node_depth - funcs that countss the levels of the Binary trees
+  * @tree: rootss node of the BT
   * Return: Nothing
   */
+
 size_t node_depth(const binary_tree_t *tree)
 {
 	if (!tree)
@@ -14,10 +15,11 @@ size_t node_depth(const binary_tree_t *tree)
 }
 
 /**
-  * binary_tree_depth - measures the depth of a node in a binary tree
-  * @tree: root node of the BT
+  * binary_tree_depth - funcs that measures the depths of a node in a binary trees
+  * @tree: roots node of the BT
   * Return: Nothing
   */
+
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	if (!tree)

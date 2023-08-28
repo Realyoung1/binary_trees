f04b50043f9fe68e729395076f20f4deab33a2f6
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-08-28 07:11:46 -0700

    binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,10 +1,11 @@
 #include "binary_trees.h"
 
 /**
-  * count_nodes - measures the height of a binary tree
-  * @tree: root node of the BT
-  * Return: the number of nodes
+  * count_nodes - funcs that measures the height of a binary treess
+  * @tree: roots nodes of the BT
+  * Return: the numbers of nodes
   */
+
 size_t count_nodes(const binary_tree_t *tree)
 {
 	size_t height = 0;
@@ -20,10 +21,11 @@ size_t count_nodes(const binary_tree_t *tree)
 }
 
 /**
-  * logbase2 - measures the height of a binary tree
-  * @nodes: root node of the BT
+  * logbase2 - funcs that measurs the heights of a binary trees
+  * @nodes: roots nodes of the BT
   * Return: Nothing
   */
+
 size_t logbase2(size_t nodes)
 {
 	size_t height = 0, pow2 = 1;
@@ -37,8 +39,8 @@ size_t logbase2(size_t nodes)
 }
 
 /**
-  * binary_tree_height - measures the height of a binary tree
-  * @tree: root node of the BT
+  * binary_tree_height - funcs that measurs the height of a binary trees
+  * @tree: roots node of the BT
   * Return: Nothing
   */
 size_t binary_tree_height(const binary_tree_t *tree)

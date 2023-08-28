49bb4141cfb1abd445a568bf983d0a0726b4376e
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-08-28 06:56:43 -0700

    binary tree

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,11 +1,12 @@
 #include "binary_trees.h"
 
 /**
-  * binary_tree_node - Create a new node of a Binary tree
-  * @parent: node parent
-  * @value: key to insert in the new node
-  * Return: the address of the new node
+  * binary_tree_node - funcs creatings a new node of a Binary trees
+  * @parent: node parentss
+  * @value: keyss to inserts in the new nodess
+  * Return: the addresss of the new nodess
   */
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = NULL;

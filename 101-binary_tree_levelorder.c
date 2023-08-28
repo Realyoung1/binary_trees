60fa9abb718eb50671b6175c87a8e1e630455e34
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-08-28 07:33:51 -0700

    binary tree

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,12 +1,13 @@
 #include "binary_trees.h"
 
 /**
-  * printGivenLevel - print in order level
-  * @tree: root node of the BT
-  * @func: function to print the traverse
-  * @level: level of BiTr
+  * printGivenLevel - funcs that print in order level
+  * @tree: rootss node of the BT
+  * @func: funcs to printss the traverse
+  * @level: levels of BiTr
   * Return: Nothing
   */
+
 void printGivenLevel(binary_tree_t *tree, void (*func)(int), int level)
 {
 	if (tree == NULL)
@@ -21,10 +22,11 @@ void printGivenLevel(binary_tree_t *tree, void (*func)(int), int level)
 }
 
 /**
-  * height - get the height of BiTr
-  * @tree: root node of the BT
+  * height - funcs get the heights of BiTr
+  * @tree: roots node of the BT
   * Return: the height of BT
   */
+
 int height(binary_tree_t *tree)
 {
 	int lheight = 0;
@@ -41,19 +43,20 @@ int height(binary_tree_t *tree)
 }
 
 /**
-  * binary_tree_levelorder - goes through abinarytreeusinglevel-ordertraversal
-  * @tree: root node of the BT
-  * @func: function to print the traverse
+  * binary_tree_levelorder - funcs that goes through abinarytreeusinglevel-ordertraversal
+  * @tree: rootss node of the BT
+  * @func: functions to print the traverse
   * Return: Nothing
   */
+
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-	int h = 0, i = 0;
+	int q = 0, w = 0;
 
 	if (tree && func)
 	{
-		h = height((binary_tree_t *) tree);
-		for (i = 1; i <= h; i++)
-			printGivenLevel((binary_tree_t *) tree, func, i);
+		q = height((binary_tree_t *) tree);
+		for (w = 1; w <= q; w++)
+			printGivenLevel((binary_tree_t *) tree, func, w);
 	}
 }

20964df8163393e1a223bbe4193e89f4002fe371
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-08-28 07:05:47 -0700

    binary tree

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,10 +1,11 @@
 #include "binary_trees.h"
 
 /**
-  * binary_tree_is_root - Check if a node is the root
-  * @node: node of the BT
+  * binary_tree_is_root - funcs Checking if a node is the rootings
+  * @node: nodess of the BT
   * Return: 1 if a node is a root otherwise 0
   */
+
 int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (!node)

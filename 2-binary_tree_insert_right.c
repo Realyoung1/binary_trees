b8830f277729abbe2770d73f9c11fa1cd6244768
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-08-28 07:03:10 -0700

    binary tree

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,11 +1,12 @@
 #include "binary_trees.h"
 
 /**
-  * binary_tree_insert_right - Insert a node on right of Binary tree
-  * @parent: node parent
-  * @value: key to insert in the new node
-  * Return: the address of the new node
+  * binary_tree_insert_right - funcs Inserting a node on right of Binary treess
+  * @parent: nodes parentss
+  * @value: keys to insert in the new nodess
+  * Return: the address of thes new nodess
   */
+
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newright_node = NULL;

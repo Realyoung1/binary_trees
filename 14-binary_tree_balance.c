575abf82de6f03ec3a8821a5e43ccada121186aa
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-08-29 11:55:52 -0700

    binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,9 +1,22 @@
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
 #include "binary_trees.h"
+
 /**
- * binary_tree_height - measures the height of a binary tree.
- *@tree: pointer to the root node of the tree to measure the height.
+ * binary_tree_height -funcs that measuring the height of a binary treess
+ *@tree: pointerss to the root node of the tree to measureing the heightss.
  * Return: if tree is NULL, your function must return 0.
  */
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree)
@@ -22,11 +35,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	else
 		return (0);
 }
+
 /**
- * binary_tree_balance - measures the balance factor of a binary tree.
- *@tree: pointer to the root node of the tree to measure the balance factor.
+ * binary_tree_balance - funcs measures the balancess factor of a binary treess
+ *@tree: pointerss to the root node of the tree to measuring the balance factorss.
  * Return: If tree is NULL, return 0
  */
+
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (tree == NULL)

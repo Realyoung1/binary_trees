3704422263711536251a27538c3da69cb6139971
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-08-29 12:08:10 -0700

    Update 102-binary_tree_is_complete.c

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,113 +1,32 @@
 #include "binary_trees.h"
-/**
- * new_node - Function that creates a new_node in a linked_list
- * @node: Type pointer of node to be created
- * Return: the node created
- */
-link_t *new_node(binary_tree_t *node)
-{
-	link_t *new;
 
-	new =  malloc(sizeof(link_t));
-	if (new == NULL)
-	{
-		return (NULL);
-	}
-	new->node = node;
-	new->next = NULL;
-
-	return (new);
-}
 /**
- * free_q - Function that free the nodes at the linked list
- * @head: Node of the linked_list
- */
-void free_q(link_t *head)
-{
-	link_t *temp_node;
-
-	while (head)
-	{
-		temp_node = head->next;
-		free(head);
-		head = temp_node;
-	}
-}
-/**
- * _push - Function that pushes a node into the stack
- * @node: Type pointer of node of the tree
- * @head: Type head node of in the stack
- * @tail: Type tail node of in the stack
- */
-void _push(binary_tree_t *node, link_t *head, link_t **tail)
-{
-	link_t *new;
-
-	new = new_node(node);
-	if (new == NULL)
-	{
-		free_q(head);
-		exit(1);
-	}
-	(*tail)->next = new;
-	*tail = new;
-}
-/**
- * _pop - Function that pops a node into the stack
- * @head: Type head node of in the stack
- */
-void _pop(link_t **head)
-{
-	link_t *temp_node;
-
-	temp_node = (*head)->next;
-	free(*head);
-	*head = temp_node;
-}
-/**
- * binary_tree_is_complete - Function that checks if a binary tree is complete
- * @tree: Type pointer of node of the tree
- * Return: 1 if is complete 0 if it is not
- */
+  * binary_tree_is_complete - checks if a binary tree is complete
+  * @tree: root node of the BT
+  * Return: 1 if is complete otherwise 0
+  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-	link_t *head, *tail;
-	int flag = 0;
+	size_t comp_l = 0, comp_r = 0, comp = 0;
 
-	if (tree == NULL)
-	{
+	if (!tree || (!tree->left && tree->right))
 		return (0);
-	}
-	head = tail = new_node((binary_tree_t *)tree);
-	if (head == NULL)
-	{
-		exit(1);
-	}
-	while (head != NULL)
-	{
-		if (head->node->left != NULL)
-		{
-			if (flag == 1)
-			{
-				free_q(head);
-				return (0);
-			}
-			_push(head->node->left, head, &tail);
-		}
-		else
-			flag = 1;
-		if (head->node->right != NULL)
-		{
-			if (flag == 1)
-			{
-				free_q(head);
-				return (0);
-			}
-			_push(head->node->right, head, &tail);
-		}
-		else
-			flag = 1;
-		_pop(&head);
-	}
-	return (1);
+
+	if (!(tree->left) && !(tree->right))
+		return (1);
+
+	comp_l = binary_tree_is_complete(tree->left);
+	comp_r = binary_tree_is_complete(tree->right);
+
+	if (tree->left && !tree->right && (comp_l == 1 && comp_r == 0))
+		return (2);
+
+	if ((comp_l == 0 && comp_r > 0) || (comp_l > 0 && comp_r == 0))
+		comp = 0;
+	else if ((comp_l == 2 && comp_r == 2) || (comp_l == 2 && comp_r == 2))
+		comp = 0;
+	else
+		comp = 1;
+
+	return (comp);
 }

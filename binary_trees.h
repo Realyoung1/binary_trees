a3a3be17e81a5e218d7b9e2d6e35904a0b74d80f
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-08-29 10:24:50 -0700

    binary tree

--- binary_trees.h
+++ binary_trees.h
@@ -3,9 +3,17 @@
 
 /* Libraries */
 
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
 #include <stdio.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
 #include <stdlib.h>
-#include <stddef.h>
 
 /* Structures */
 

95b533e16c41d661c6585f52aa615a3793298382
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-08-29 10:31:53 -0700

    binary tree

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -12,7 +12,7 @@
 #include "binary_trees.h"
 
 /**
-  * binary_tree_preorder - funcs that Goes through a binary treess using pre-order traversalss
+  * binary_tree_preorder - funcs that Goes through a binary treess using pre-order traver
   * @tree: rootss node of the BT
   * @func: functionss to print the traverses
   * Return: Nothings

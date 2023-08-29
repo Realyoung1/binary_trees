93f12ce80577df03c6a482584cbd23aa420f2a17
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-08-29 10:38:36 -0700

    binary tree

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,3 +1,15 @@
+
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
 #include "binary_trees.h"
 
 /**
